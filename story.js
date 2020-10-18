df6de32f2738eb496bf042079c2f67e054d522a9
Rahaf Alenezi <ralenezi71@gmail.com>
2020-10-18 19:13:10 +0300

    done

---
+++ story.js
@@ -0,0 +1,8 @@
+let name = 'Rahaf'
+let age = 23
+hobby = 'time wasting'
+console.log(
+  `Mr./Mrs. ${name}, in 33 years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+)
